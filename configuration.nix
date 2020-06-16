6e11d4e892cf72e2a77e764d93430f4be67950bc
Chris Bailey <me@cbailey.co.uk>
2020-06-16 16:07:42 +0100

    Simplify configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
@@ -14,41 +10,23 @@
     allowUnfree = true;
   };
 
-
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda";
 
-  networking.hostName = "tteokbokki"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "tteokbokki";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.ens33.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/London";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     wget
     unzip
@@ -60,13 +38,9 @@
     konsole
     git
     firefox-bin
-
-    # Swag
-    pfetch
+    home-manager
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs.zsh = {
     enable = true;
     shellAliases = {
@@ -74,59 +48,25 @@
     };
     enableCompletion = true;
     autosuggestions.enable = true;
-
-    interactiveShellInit = ''
-      export ZDOTDIR=${pkgs.zsh-prezto}/
-      export NIXOS=/etc/nixos/
-      export ZSHCONFIG=$NIXOS/dotfiles/zsh
-
-      source "$ZSHCONFIG/zpreztorc"
-      source "$ZDOTDIR/init.zsh"
-      source "$ZSHCONFIG/zsh_functions"
-
-      HISTFILE=~/.zsh_history
-    '';
-    
-    promptInit = ''
-      autoload -U promptinit && promptinit && prompt ziggy_nerd 
-    '';
   };
 
-  # Font configuration
   fonts = {
     enableFontDir = true;
     fonts = with pkgs; [
       corefonts
       fira
+      fira-code-symbols
+      font-awesome-ttf
       unifont
     ];
   };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "us";
   services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
 
   # Override nixos DWM with personal build
   nixpkgs.overlays = [
@@ -144,23 +84,14 @@
   services.xserver.displayManager.lightdm.autoLogin.user = "chris";
   services.xserver.windowManager.dwm.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.chris = {
     isNormalUser = true;
     extraGroups = [ "sound" "wheel" ];
     shell = pkgs.zsh;
   };
 
-  # Enable VMWare guest additions
   virtualisation.vmware.guest.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
-
+  system.stateVersion = "20.03";
 }
 
