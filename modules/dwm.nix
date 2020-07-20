e2f431a66993177400ee09cc3404b290fbf8a58f
Chris Bailey <me@cbailey.co.uk>
2020-07-20 10:48:36 +0100

    Move commented compton config to dwm.nix

--- modules/dwm.nix
+++ modules/dwm.nix
@@ -15,13 +15,19 @@ with lib;
     };
   
     config = mkIf cfg.enable (mkMerge [{
+      # my dwm config requires dmenu
+      # for which rofi is a replacement of
+      environment.systemPackages = with pkgs; [
+        rofi
+      ];
+
       # Grab my custom dwm build
       nixpkgs.overlays = [
         (self: super: {
           dwm = super.dwm.overrideAttrs(_: {
             src = builtins.fetchGit {
               url = "https://github.com/vereis/dwm";
-              rev = "101300dc26467cb9c474dde946655ad68528ae35";
+              rev = "b247aeb8e713ac5c644c404fa1384e05e0b8bc6f";
               ref = "master";
             };
           });
@@ -30,5 +36,8 @@ with lib;
 
       services.xserver.enable = true;
       services.xserver.windowManager.dwm.enable = true;
+      services.xserver.displayManager.sessionCommands = ''
+        xsetroot --solid "#222222"
+      '';
     }]);
   }
--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -7,7 +7,6 @@
   hardware.pulseaudio.enable = true;
 
   home-manager.users.chris.home.packages = with pkgs; [
-    #pkgs.compton
     pkgs.ctags
     pkgs.neofetch
 
@@ -20,9 +19,4 @@
   home-manager.users.chris.programs.home-manager.enable = true;
   home-manager.users.chris.programs.irssi.enable = true;
   home-manager.users.chris.programs.firefox.enable = true;
-
-  #home-manager.users.chris.services.picom.enable = true;
-  #home-manager.users.chris.services.picom.backend = "xr_glx_hybrid";
-  #home-manager.users.chris.services.picom.vSync = true;
-  #home-manager.users.chris.services.picom.inactiveDim = "0.133";
 }
