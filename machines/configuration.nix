ec7e70a3a26944cc94652bce6ad832636808c831
Chris Bailey <me@cbailey.co.uk>
2022-08-03 13:24:35 +0100

    Switch to flakes, bootstrap hosts from scratch.

---
+++ machines/configuration.nix
@@ -0,0 +1,92 @@
+{ config, lib, pkgs, inputs, username, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  users.users.${username} = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    shell = pkgs.zsh;
+  };
+
+  environment.systemPackages = with pkgs; [
+      acpi
+      cacert
+      git
+      htop
+      httpie
+      killall
+      openssh
+      openssl
+      pciutils
+      pfetch
+      tmux
+      tree
+      usbutils
+      wget
+      xclip
+    ];
+
+  time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_US.utf8";
+
+  nix = {
+    package = pkgs.nixFlakes;
+    settings.auto-optimise-store = true;
+    registry.nixpkgs.flake = inputs.nixpkgs;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  services = {
+    getty.autologinUser = "${username}";
+
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+
+    # Allow Home-Manager to control starting WMs via XSession
+    xserver = {
+      enable = true;
+      layout = "us";
+      xkbVariant = "";
+      libinput.enable = true;
+
+      displayManager = {
+        defaultSession = "xsession";
+        autoLogin.user = "${username}";
+
+        session = [{
+          manage = "desktop";
+          name = "xsession";
+          start = ''
+	          exec $HOME/.xsession &
+	          waitPID=$!
+	        '';
+        }];
+      };
+    };
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+
+  security.rtkit.enable = true;
+
+  programs.ssh.askPassword = "";
+
+  virtualisation = {
+    docker = {
+      enable = true;
+      enableOnBoot = true;
+    };
+  };
+
+  system.stateVersion = "22.11";
+}
---
+++ modules/home/awesome.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+{
+  options.modules.awesome = {
+    enable = mkOption   { type = types.bool; default = false; };
+    fontSize = mkOption { type = types.int;  default = 11; };
+  };
+
+  config = mkIf config.modules.awesome.enable {
+    home.packages = with pkgs; [ awesome rofi fantasque-sans-mono ];
+
+    home.file.".config/awesome" = {
+      recursive = true;
+      source    = ./awesome;
+    };
+
+    programs.rofi = {
+      enable = true;
+      font = "Fantasque Sans Mono ${toString config.modules.awesome.fontSize}";
+      location = "center";
+      theme = {
+        "*" = { border = 0; margin = 0; padding = config.modules.awesome.fontSize / 2; spacing = 0; };  
+      };
+    };
+
+    xsession.enable = true;
+    xsession.windowManager.awesome.enable = true;
+
+    services.picom = {
+      enable = true;
+      shadow = false;
+      settings = {
+        inactive-dim = 0.2;
+      };
+    };
+  };
+}
