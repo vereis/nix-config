f6349897c491e426f3d35ffeefb9c154d8c56748
Chris Bailey <me@cbailey.co.uk>
2022-09-04 16:25:15 +0100

    Remove outer gaps via negative screen padding

--- modules/home/awesome/rc.lua
+++ modules/home/awesome/rc.lua
@@ -74,6 +74,7 @@ end
 
 awful.screen.connect_for_each_screen(function(s)
 	set_wallpaper(s)
+	s.padding = -2
 	awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 end)
 
