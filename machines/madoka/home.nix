05b01cf19fb3491d5ad3517f299045d6a593e17c
Chris Bailey <me@cbailey.co.uk>
2023-05-08 00:21:56 +0100

    Move ranger to a real module

--- machines/madoka/home.nix
+++ machines/madoka/home.nix
@@ -8,6 +8,7 @@
     ../../modules/home/kitty.nix
     ../../modules/home/ncmpcpp.nix
     ../../modules/home/neovim.nix
+    ../../modules/home/ranger.nix
     ../../modules/home/vetspire.nix
     ../../modules/home/zsh.nix
   ];
@@ -18,8 +19,9 @@
   modules.git.enable        = true;
   modules.kitty.enable      = true;
   modules.kitty.fontSize    = 13;
+  modules.ncmpcpp.enable    = true;
   modules.neovim.enable     = true;
+  modules.ranger.enable     = true;
   modules.vetspire.enable   = true;
   modules.zsh.enable        = true;
-  modules.ncmpcpp.enable    = true;
 }
