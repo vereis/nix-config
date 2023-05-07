1625c2689f7f27375c2bcb62c7b6965a8b0a1036
Chris Bailey <me@cbailey.co.uk>
2023-05-07 21:06:55 +0100

    Write new firefox module

--- machines/madoka/home.nix
+++ machines/madoka/home.nix
@@ -3,18 +3,18 @@
 {
   imports = [
     ../../modules/home/awesome.nix
-    ../../modules/home/edge.nix
+    ../../modules/home/firefox.nix
     ../../modules/home/git.nix
     ../../modules/home/kitty.nix
+    ../../modules/home/ncmpcpp.nix
     ../../modules/home/neovim.nix
     ../../modules/home/vetspire.nix
     ../../modules/home/zsh.nix
-    ../../modules/home/ncmpcpp.nix
   ];
 
   modules.awesome.enable    = true;
   modules.awesome.fontSize  = 13;
-  modules.edge.enable       = true;
+  modules.firefox.enable    = true;
   modules.git.enable        = true;
   modules.kitty.enable      = true;
   modules.kitty.fontSize    = 13;
