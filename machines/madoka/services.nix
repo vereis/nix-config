5172691079f63650b48db5a0ba1a292c6ce95d2d
Chris Bailey <me@cbailey.co.uk>
2023-06-19 14:38:37 +0100

    Connect to printer network

--- machines/madoka/services.nix
+++ machines/madoka/services.nix
@@ -3,9 +3,13 @@
 {
   imports = [
     ../../modules/services/tailscale.nix
+    ../../modules/services/printing.nix
   ];
 
   modules.tailscale.enable = true;
   modules.tailscale.ssh.enable = true;
   modules.tailscale.openFirewall = true;
+
+  modules.printing.enable = true;
+  modules.printing.wifi = true;
 }
