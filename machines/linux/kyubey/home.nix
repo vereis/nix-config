f0045f7310f8d600b63e91c1d9d81b136c487787
vereis <me@vereis.com>
2025-07-31 13:00:40 +0100

    update kyubey configuration

--- machines/linux/kyubey/home.nix
+++ machines/linux/kyubey/home.nix
@@ -2,24 +2,15 @@
 
 {
   imports = [
-    ../../../modules/home/awesome.nix
-    ../../../modules/home/firefox.nix
-    ../../../modules/home/git.nix
-    ../../../modules/home/kitty.nix
-    ../../../modules/home/ncmpcpp.nix
-    ../../../modules/home/neovim.nix
-    ../../../modules/home/ranger.nix
-    ../../../modules/home/zsh.nix
+    ../../../modules/home/tui.nix
   ];
 
-  modules.awesome.enable = true;
-  modules.awesome.fontSize = 13;
-  modules.firefox.enable = true;
-  modules.git.enable = true;
-  modules.kitty.enable = true;
-  modules.kitty.fontSize = 13;
-  modules.ncmpcpp.enable = true;
-  modules.neovim.enable = true;
-  modules.ranger.enable = true;
-  modules.zsh.enable = true;
+  modules.tui = {
+    enable = true;
+    extraPackages = with pkgs; [
+      yt-dlp
+      claude-code
+      gemini-cli
+    ];
+  };
 }
