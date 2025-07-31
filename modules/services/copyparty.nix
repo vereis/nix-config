514184cdf6d421a030ba5871da407ec12eaa390e
vereis <me@vereis.com>
2025-07-31 22:26:25 +0100

    chore(copyparty): update copyparty service

--- modules/services/copyparty.nix
+++ modules/services/copyparty.nix
@@ -4,11 +4,57 @@ with lib;
 {
   options.modules.copyparty = {
     enable = mkOption { type = types.bool; default = false; };
+
+    accounts = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          password = mkOption {
+            type = types.str;
+            description = "Password for the account";
+          };
+        };
+      });
+      default = {};
+      description = "User accounts configuration";
+    };
+
+    volumes = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          path = mkOption {
+            type = types.str;
+            description = "Directory path to share";
+          };
+          access = mkOption {
+            type = types.attrsOf (types.either types.str (types.listOf types.str));
+            default = {};
+            description = "Access permissions configuration";
+          };
+          flags = mkOption {
+            type = types.attrsOf types.anything;
+            default = {};
+            description = "Volume-specific flags";
+          };
+        };
+      });
+      default = {};
+      description = "Volume configuration";
+    };
   };
 
   config = mkIf config.modules.copyparty.enable {
     nixpkgs.overlays = [ copyparty.overlays.default ];
     environment.systemPackages = with pkgs; [ copyparty  ];
+
+    # Create password files for each account
+    environment.etc = mapAttrs' (username: accountConfig:
+      nameValuePair "copyparty/${username}.passwd" {
+        text = accountConfig.password;
+        mode = "0600";
+        user = "copyparty";
+        group = "copyparty";
+      }
+    ) config.modules.copyparty.accounts;
 
     services.copyparty = {
       enable = true;
@@ -21,19 +67,12 @@ with lib;
         ignored-flag = false;
       };
 
-      volumes = {
-        "/" = {
-          path = "/storage";
-          access = { rw = "*"; };
+      # Configure accounts with password files
+      accounts = mapAttrs (username: accountConfig: {
+        passwordFile = "/etc/copyparty/${username}.passwd";
+      }) config.modules.copyparty.accounts;
 
-          flags = {
-            fk = 4;
-            scan = 60;
-            e2d = true;
-            nohash = "\.iso$";
-          };
-        };
-      };
+      volumes = config.modules.copyparty.volumes;
     };
   };
 }
