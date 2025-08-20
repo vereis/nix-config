b6f151aaf56e79e10e0adbc829b4d53cec7eeae2
vereis <me@vereis.com>
2025-08-20 11:42:12 +0100

    Default browser to edge on windows hosts

--- machines/windows/configuration.nix
+++ machines/windows/configuration.nix
@@ -5,4 +5,5 @@
   wsl.enable = true;
 
   environment.systemPackages = with pkgs; [ xclip ];
+  environment.variables.BROWSER = "/mnt/c/Program\\ Files\\ \\(x86\\)/Microsoft/Edge/Application/msedge.exe";
 }
