95ba3f931aa1099e2385e58ce77e80426200c77e
vereis <me@vereis.com>
2025-09-18 16:16:51 +0100

    [CHORE] update kyubey and madoka machines to use extraSessionVariables for secrets

--- machines/windows/madoka/home.nix
+++ machines/windows/madoka/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, secrets, ... }:
 
 {
   imports = [
@@ -15,6 +15,10 @@
     ];
     extraFiles = {
       "./.claude/" = { recursive = true; source = ../../../modules/home/tui/claude; };
+    };
+    extraSessionVariables = {
+      GOOGLE_CLOUD_PROJECT = secrets.gemini-cli.googleCloudProject;
+      JIRA_API_TOKEN = secrets.vetspire.jiraApiKey;
     };
   };
 }
