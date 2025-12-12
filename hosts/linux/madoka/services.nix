9cb7ba08dfde8d4ebe19922b176889a96db956af
vereis <me@vereis.com>
2025-12-12 12:02:30 +0000

    chore(gnome): refactor desktop configuration into reusable GNOME and graphics service modules

--- hosts/linux/madoka/services.nix
+++ hosts/linux/madoka/services.nix
@@ -3,9 +3,25 @@
 {
   imports = [
     ../../../modules/services/tailscale.nix
+    ../../../modules/services/gnome.nix
+    ../../../modules/services/graphics.nix
   ];
 
   modules = {
     tailscale.enable = true;
+
+    services.desktop = {
+      gnome = {
+        enable = true;
+        altDrag = true;
+        extraGSettings = {
+          "org.gnome.desktop.session" = {
+            # Disable idle timeout (prevents screen from auto-dimming/locking)
+            idle-delay = "uint32 0";
+          };
+        };
+      };
+      graphics.enable = true;
+    };
   };
 }
