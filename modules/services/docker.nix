dd48b57b176fa4cc712deeaf9adfea327fe14f41
vereis <me@vereis.com>
2025-12-15 14:20:44 +0000

    [CHORE] Extract Docker configuration into reusable service module

---
+++ modules/services/docker.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  lib,
+  config,
+  username,
+  ...
+}:
+
+with lib;
+{
+  options.modules.docker = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    rootless = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable rootless Docker mode";
+    };
+    autoPrune = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable weekly auto-pruning of unused images";
+    };
+    extraUsers = mkOption {
+      type = types.listOf types.str;
+      default = [ ];
+      description = "Additional users to add to docker group (current user always included)";
+    };
+  };
+
+  config = mkIf config.modules.docker.enable {
+    virtualisation.docker = mkMerge [
+      { enable = true; }
+      (mkIf config.modules.docker.rootless {
+        rootless.enable = true;
+        rootless.setSocketVariable = true;
+      })
+      (mkIf config.modules.docker.autoPrune {
+        autoPrune.enable = true;
+        autoPrune.dates = "weekly";
+      })
+    ];
+
+    users.users = listToAttrs (
+      map (user: {
+        name = user;
+        value = {
+          extraGroups = [ "docker" ];
+        };
+      }) ([ username ] ++ config.modules.docker.extraUsers)
+    );
+  };
+}
