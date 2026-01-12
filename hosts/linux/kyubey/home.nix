d688832e40573db53c60155a9b2e6cb555fc9e92
vereis <me@vereis.com>
2026-01-12 11:51:37 +0000

    chore(opencode): remove from host configs

--- hosts/linux/kyubey/home.nix
+++ hosts/linux/kyubey/home.nix
@@ -9,14 +9,8 @@
     enable = true;
     extraPackages = with pkgs; [
       yt-dlp
-      opencode
     ];
-    extraFiles = {
-      "./.config/opencode/" = {
-        recursive = true;
-        source = ../../../modules/home/tui/opencode;
-      };
-    };
+    extraFiles = { };
     extraSessionVariables = {
       GOOGLE_CLOUD_PROJECT = secrets.gemini-cli.googleCloudProject;
       JIRA_API_TOKEN = secrets.vetspire.jiraApiKey;
