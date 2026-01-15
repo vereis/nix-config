affda65cabd7859fd028fe1b1a27cb908086ca42
vereis <me@vereis.com>
2026-01-15 16:46:30 +0000

    refactor(gui): make terminal app hiding more declarative
    Refactors xdg.desktopEntries to use a simple list of terminal apps
    that gets mapped into desktop entry overrides, instead of repeating
    the same structure for each app.
    
    Also expands the list to include common terminal applications:
    htop, vim, vi, tmux, zellij, ranger, midnight commander, nano, emacs.
    
    Makes it trivial to add new apps to hide - just add one line to the list.

--- modules/home/gui.nix
+++ modules/home/gui.nix
@@ -281,23 +281,29 @@ in
       };
 
     # Hide terminal-only applications from the GUI application launcher
-    xdg.desktopEntries = {
-      btop = {
-        name = "btop";
-        noDisplay = true;
-      };
-      lf = {
-        name = "lf";
-        noDisplay = true;
-      };
-      xterm = {
-        name = "XTerm";
-        noDisplay = true;
-      };
-      nvim = {
-        name = "Neovim";
-        noDisplay = true;
-      };
-    };
+    # Add any terminal apps that shouldn't appear in the launcher to this list
+    xdg.desktopEntries = builtins.listToAttrs (
+      map (app: {
+        name = app;
+        value = {
+          name = app;
+          noDisplay = true;
+        };
+      }) [
+        "btop"
+        "htop"
+        "lf"
+        "nvim"
+        "vim"
+        "vi"
+        "xterm"
+        "tmux"
+        "zellij"
+        "ranger"
+        "mc" # midnight commander
+        "nano"
+        "emacs" # terminal version
+      ]
+    );
   };
 }
