16056e8a807656ade07442d507796d7960c3bd0d
vereis <me@vereis.com>
2026-01-15 11:09:58 +0000

    feat(iroha): add XPS 13 2025 NixOS host
    - add hardware-configuration.nix for Intel CPU (XPS 13 2025)
    - add default.nix with systemd-boot and networkmanager
    - add services.nix with GNOME desktop, Intel graphics, Tailscale, Docker, Steam
    - add home.nix with TUI and GUI modules
    - register host in parts/hosts.nix
    - add to CI build matrix

---
+++ hosts/linux/iroha/home.nix
@@ -0,0 +1,33 @@
+{
+  pkgs,
+  secrets,
+  ...
+}:
+{
+  imports = [
+    ../../../modules/home/tui.nix
+    ../../../modules/home/gui.nix
+  ];
+
+  modules.tui = {
+    enable = true;
+    extraPackages = with pkgs; [
+      yt-dlp
+      tealdeer
+      bsd-finger
+      jira-cli-go
+    ];
+    extraSessionVariables = {
+      GOOGLE_CLOUD_PROJECT = secrets.gemini-cli.googleCloudProject;
+      JIRA_API_TOKEN = secrets.vetspire.jiraApiKey;
+    };
+  };
+
+  modules.gui = {
+    enable = true;
+    extraPackages = with pkgs; [
+      slack
+      teams-for-linux
+    ];
+  };
+}
