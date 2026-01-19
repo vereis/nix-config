ed19612bf3c5251afc47362b66c72e48f2b46766
vereis <me@vereis.com>
2026-01-19 14:27:27 +0000

    feat(fish): change to starship

--- modules/home/tui.nix
+++ modules/home/tui.nix
@@ -55,7 +55,6 @@ with lib;
           git
           httpie
           jq
-          kubectl
           killall
           lsof
           nerdfetch
@@ -112,6 +111,17 @@ with lib;
           };
           ".local/share/atuin/key" = {
             text = secrets.atuin.key;
+          };
+          ".config/fish/conf.d/starship_transient.fish" = {
+            text = ''
+              # Enable transient prompt after Starship is loaded
+              function __enable_transience_on_prompt --on-event fish_prompt
+                if type -q enable_transience
+                  enable_transience
+                  functions --erase __enable_transience_on_prompt
+                end
+              end
+            '';
           };
         }
         config.modules.tui.extraFiles
@@ -180,179 +190,242 @@ with lib;
           set fish_cursor_insert block blink
           set fish_cursor_replace_one block blink
           set fish_cursor_visual block blink
-
-          # Tide prompt configuration
-          set -g tide_aws_bg_color normal
-          set -g tide_aws_color yellow
-          set -g tide_aws_icon \uf270
-          set -g tide_bun_bg_color normal
-          set -g tide_bun_color white
-          set -g tide_bun_icon \U000f0cd3
-          set -g tide_character_color brgreen
-          set -g tide_character_color_failure brred
-          set -g tide_character_icon \u276f
-          set -g tide_character_vi_icon_default \u276e
-          set -g tide_character_vi_icon_replace \u25b6
-          set -g tide_character_vi_icon_visual V
-          set -g tide_cmd_duration_bg_color normal
-          set -g tide_cmd_duration_color brblack
-          set -g tide_cmd_duration_decimals 0
-          set -g tide_cmd_duration_icon \x1d
-          set -g tide_cmd_duration_threshold 3000
-          set -g tide_context_always_display false
-          set -g tide_context_bg_color normal
-          set -g tide_context_color_default yellow
-          set -g tide_context_color_root bryellow
-          set -g tide_context_color_ssh yellow
-          set -g tide_context_hostname_parts 1
-          set -g tide_crystal_bg_color normal
-          set -g tide_crystal_color brwhite
-          set -g tide_crystal_icon \ue62f
-          set -g tide_direnv_bg_color normal
-          set -g tide_direnv_bg_color_denied normal
-          set -g tide_direnv_color bryellow
-          set -g tide_direnv_color_denied brred
-          set -g tide_direnv_icon \u25bc
-          set -g tide_distrobox_bg_color normal
-          set -g tide_distrobox_color brmagenta
-          set -g tide_distrobox_icon \U000f01a7
-          set -g tide_docker_bg_color normal
-          set -g tide_docker_color blue
-          set -g tide_docker_default_contexts default\x1ecolima
-          set -g tide_docker_icon \uf308
-          set -g tide_elixir_bg_color normal
-          set -g tide_elixir_color magenta
-          set -g tide_elixir_icon \ue62d
-          set -g tide_gcloud_bg_color normal
-          set -g tide_gcloud_color blue
-          set -g tide_gcloud_icon \U000f02ad
-          set -g tide_git_bg_color normal
-          set -g tide_git_bg_color_unstable normal
-          set -g tide_git_bg_color_urgent normal
-          set -g tide_git_color_branch brgreen
-          set -g tide_git_color_conflicted brred
-          set -g tide_git_color_dirty bryellow
-          set -g tide_git_color_operation brred
-          set -g tide_git_color_staged bryellow
-          set -g tide_git_color_stash brgreen
-          set -g tide_git_color_untracked brblue
-          set -g tide_git_color_upstream brgreen
-          set -g tide_git_icon \x1d
-          set -g tide_git_truncation_length 24
-          set -g tide_git_truncation_strategy \x1d
-          set -g tide_go_bg_color normal
-          set -g tide_go_color brcyan
-          set -g tide_go_icon \ue627
-          set -g tide_java_bg_color normal
-          set -g tide_java_color yellow
-          set -g tide_java_icon \ue256
-          set -g tide_jobs_bg_color normal
-          set -g tide_jobs_color green
-          set -g tide_jobs_icon \uf013
-          set -g tide_jobs_number_threshold 1000
-          set -g tide_kubectl_bg_color normal
-          set -g tide_kubectl_color blue
-          set -g tide_kubectl_icon \U000f10fe
-          set -g tide_left_prompt_frame_enabled false
-          set -g tide_left_prompt_items pwd\x1egit\x1echaracter
-          set -g tide_left_prompt_prefix
-          set -g tide_left_prompt_separator_diff_color \x20
-          set -g tide_left_prompt_separator_same_color \x20
-          set -g tide_left_prompt_suffix
-          set -g tide_nix_shell_bg_color normal
-          set -g tide_nix_shell_color brblue
-          set -g tide_nix_shell_icon \uf313
-          set -g tide_node_bg_color normal
-          set -g tide_node_color green
-          set -g tide_node_icon \ue24f
-          set -g tide_os_bg_color normal
-          set -g tide_os_color brwhite
-          set -g tide_os_icon \uf313
-          set -g tide_php_bg_color normal
-          set -g tide_php_color blue
-          set -g tide_php_icon \ue608
-          set -g tide_private_mode_bg_color normal
-          set -g tide_private_mode_color brwhite
-          set -g tide_private_mode_icon \U000f05f9
-          set -g tide_prompt_add_newline_before false
-          set -g tide_prompt_color_frame_and_connection brblack
-          set -g tide_prompt_color_separator_same_color brblack
-          set -g tide_prompt_icon_connection \x20
-          set -g tide_prompt_min_cols 34
-          set -g tide_prompt_pad_items false
-          set -g tide_prompt_transient_enabled true
-          set -g tide_pulumi_bg_color normal
-          set -g tide_pulumi_color yellow
-          set -g tide_pulumi_icon \uf1b2
-          set -g tide_pwd_bg_color normal
-          set -g tide_pwd_color_anchors brcyan
-          set -g tide_pwd_color_dirs cyan
-          set -g tide_pwd_color_truncated_dirs magenta
-          set -g tide_pwd_icon \x1d
-          set -g tide_pwd_icon_home \x1d
-          set -g tide_pwd_icon_unwritable \uf023
-          set -g tide_pwd_markers .bzr\x1e.citc\x1e.git\x1e.hg\x1e.node-version\x1e.python-version\x1e.ruby-version\x1e.shorten_folder_marker\x1e.svn\x1e.terraform\x1ebun.lockb\x1eCargo.toml\x1ecomposer.json\x1eCVS\x1ego.mod\x1epackage.json\x1ebuild.zig
-          set -g tide_python_bg_color normal
-          set -g tide_python_color cyan
-          set -g tide_python_icon \U000f0320
-          set -g tide_right_prompt_frame_enabled false
-          set -g tide_right_prompt_items status\x1ecmd_duration\x1econtext\x1ejobs\x1edirenv\x1ebun\x1enode\x1epython\x1erustc\x1ejava\x1ephp\x1epulumi\x1eruby\x1ego\x1egcloud\x1ekubectl\x1edistrobox\x1etoolbox\x1eterraform\x1eaws\x1enix_shell\x1ecrystal\x1eelixir\x1ezig\x1etime
-          set -g tide_right_prompt_prefix \x20
-          set -g tide_right_prompt_separator_diff_color \x20
-          set -g tide_right_prompt_separator_same_color \x20
-          set -g tide_right_prompt_suffix
-          set -g tide_ruby_bg_color normal
-          set -g tide_ruby_color red
-          set -g tide_ruby_icon \ue23e
-          set -g tide_rustc_bg_color normal
-          set -g tide_rustc_color red
-          set -g tide_rustc_icon \ue7a8
-          set -g tide_shlvl_bg_color normal
-          set -g tide_shlvl_color yellow
-          set -g tide_shlvl_icon \uf120
-          set -g tide_shlvl_threshold 1
-          set -g tide_status_bg_color normal
-          set -g tide_status_bg_color_failure normal
-          set -g tide_status_color green
-          set -g tide_status_color_failure red
-          set -g tide_status_icon \u2714
-          set -g tide_status_icon_failure \u2718
-          set -g tide_terraform_bg_color normal
-          set -g tide_terraform_color magenta
-          set -g tide_terraform_icon \U000f1062
-          set -g tide_time_bg_color normal
-          set -g tide_time_color brblack
-          set -g tide_time_format %T
-          set -g tide_toolbox_bg_color normal
-          set -g tide_toolbox_color magenta
-          set -g tide_toolbox_icon \ue24f
-          set -g tide_vi_mode_bg_color_default normal
-          set -g tide_vi_mode_bg_color_insert normal
-          set -g tide_vi_mode_bg_color_replace normal
-          set -g tide_vi_mode_bg_color_visual normal
-          set -g tide_vi_mode_color_default white
-          set -g tide_vi_mode_color_insert cyan
-          set -g tide_vi_mode_color_replace green
-          set -g tide_vi_mode_color_visual yellow
-          set -g tide_vi_mode_icon_default D
-          set -g tide_vi_mode_icon_insert I
-          set -g tide_vi_mode_icon_replace R
-          set -g tide_vi_mode_icon_visual V
-          set -g tide_zig_bg_color normal
-          set -g tide_zig_color yellow
-          set -g tide_zig_icon \ue6a9
         '';
 
         shellAliases = {
           vim = "nvim";
         };
 
-        plugins = [
-          {
-            name = "tide";
-            inherit (pkgs.fishPlugins.tide) src;
-          }
-        ];
+        plugins = [ ];
+
+        functions = {
+          starship_transient_prompt_func = "starship module character";
+        };
+      };
+
+      starship = {
+        enable = true;
+        enableFishIntegration = true;
+        settings = {
+          "$schema" = "https://starship.rs/config-schema.json";
+          add_newline = false;
+          continuation_prompt = "[.](bright-black) ";
+
+          character = {
+            success_symbol = "[>](bold green)";
+            error_symbol = "[x](bold red)";
+            vimcmd_symbol = "[<](bold green)";
+            vimcmd_visual_symbol = "[<](bold yellow)";
+            vimcmd_replace_symbol = "[<](bold purple)";
+            vimcmd_replace_one_symbol = "[<](bold purple)";
+          };
+
+          git_commit = {
+            tag_symbol = " tag ";
+          };
+
+          git_status = {
+            ahead = ">";
+            behind = "<";
+            diverged = "<>";
+            renamed = "r";
+            deleted = "x";
+          };
+
+          aws.symbol = "aws ";
+          azure.symbol = "az ";
+
+          battery = {
+            full_symbol = "full ";
+            charging_symbol = "charging ";
+            discharging_symbol = "discharging ";
+            unknown_symbol = "unknown ";
+            empty_symbol = "empty ";
+          };
+
+          buf.symbol = "buf ";
+          bun.symbol = "bun ";
+          c.symbol = "C ";
+          cmake.symbol = "cmake ";
+          cobol.symbol = "cobol ";
+          conda.symbol = "conda ";
+          container.symbol = "container ";
+          crystal.symbol = "cr ";
+          daml.symbol = "daml ";
+          dart.symbol = "dart ";
+          deno.symbol = "deno ";
+
+          directory = {
+            read_only = " ro";
+          };
+
+          docker_context.symbol = "docker ";
+
+          dotnet = {
+            format = "via [$symbol($version )(target $tfm )]($style)";
+            symbol = ".NET ";
+          };
+
+          elixir.symbol = "exs ";
+          elm.symbol = "elm ";
+          erlang.symbol = "erl ";
+          fennel.symbol = "fnl ";
+          fortran.symbol = "fortran ";
+
+          fossil_branch = {
+            symbol = "fossil ";
+            truncation_symbol = "...";
+          };
+
+          gcloud = {
+            symbol = "gcp ";
+            disabled = true;
+          };
+
+          git_branch = {
+            symbol = "git ";
+            truncation_symbol = "...";
+          };
+
+          gleam.symbol = "gleam ";
+          golang.symbol = "go ";
+          gradle.symbol = "gradle ";
+          guix_shell.symbol = "guix ";
+          haskell.symbol = "haskell ";
+          haxe.symbol = "hx ";
+          helm.symbol = "helm ";
+
+          hg_branch = {
+            symbol = "hg ";
+            truncation_symbol = "...";
+          };
+
+          hostname.ssh_symbol = "ssh ";
+          java.symbol = "java ";
+          jobs.symbol = "*";
+          julia.symbol = "jl ";
+          kotlin.symbol = "kt ";
+          kubernetes.symbol = "kubernetes ";
+          lua.symbol = "lua ";
+          nodejs.symbol = "nodejs ";
+          memory_usage.symbol = "memory ";
+
+          meson = {
+            symbol = "meson ";
+            truncation_symbol = "...";
+          };
+
+          mojo.symbol = "mojo ";
+          nats.symbol = "nats ";
+          netns.symbol = "netns ";
+          nim.symbol = "nim ";
+          nix_shell.symbol = "nix ";
+          ocaml.symbol = "ml ";
+          odin.symbol = "odin ";
+          opa.symbol = "opa ";
+          openstack.symbol = "openstack ";
+
+          os.symbols = {
+            AIX = "aix ";
+            Alpaquita = "alq ";
+            AlmaLinux = "alma ";
+            Alpine = "alp ";
+            ALTLinux = "alt ";
+            Amazon = "amz ";
+            Android = "andr ";
+            AOSC = "aosc ";
+            Arch = "rch ";
+            Artix = "atx ";
+            Bluefin = "blfn ";
+            CachyOS = "cach ";
+            CentOS = "cent ";
+            Debian = "deb ";
+            DragonFly = "dfbsd ";
+            Elementary = "elem ";
+            Emscripten = "emsc ";
+            EndeavourOS = "ndev ";
+            Fedora = "fed ";
+            FreeBSD = "fbsd ";
+            Garuda = "garu ";
+            Gentoo = "gent ";
+            HardenedBSD = "hbsd ";
+            Illumos = "lum ";
+            Ios = "ios ";
+            InstantOS = "inst ";
+            Kali = "kali ";
+            Linux = "lnx ";
+            Mabox = "mbox ";
+            Macos = "mac ";
+            Manjaro = "mjo ";
+            Mariner = "mrn ";
+            MidnightBSD = "mid ";
+            Mint = "mint ";
+            NetBSD = "nbsd ";
+            NixOS = "nix ";
+            Nobara = "nbra ";
+            OpenBSD = "obsd ";
+            OpenCloudOS = "ocos ";
+            openEuler = "oeul ";
+            openSUSE = "osuse ";
+            OracleLinux = "orac ";
+            PikaOS = "pika ";
+            Pop = "pop ";
+            Raspbian = "rasp ";
+            Redhat = "rhl ";
+            RedHatEnterprise = "rhel ";
+            RockyLinux = "rky ";
+            Redox = "redox ";
+            Solus = "sol ";
+            SUSE = "suse ";
+            Ubuntu = "ubnt ";
+            Ultramarine = "ultm ";
+            Unknown = "unk ";
+            Uos = "uos ";
+            Void = "void ";
+            Windows = "win ";
+            Zorin = "zorn ";
+          };
+
+          package.symbol = "pkg ";
+          perl.symbol = "pl ";
+          php.symbol = "php ";
+
+          pijul_channel = {
+            symbol = "pijul ";
+            truncation_symbol = "...";
+          };
+
+          pixi.symbol = "pixi ";
+          pulumi.symbol = "pulumi ";
+          purescript.symbol = "purs ";
+          python.symbol = "py ";
+          quarto.symbol = "quarto ";
+          raku.symbol = "raku ";
+          red.symbol = "red ";
+          rlang.symbol = "r ";
+          ruby.symbol = "rb ";
+          rust.symbol = "rs ";
+          scala.symbol = "scala ";
+          shlvl.symbol = "shlvl ";
+          spack.symbol = "spack ";
+          solidity.symbol = "solidity ";
+
+          status = {
+            symbol = "[x](bold red) ";
+            not_executable_symbol = "noexec";
+            not_found_symbol = "notfound";
+            sigint_symbol = "sigint";
+            signal_symbol = "sig";
+          };
+
+          sudo.symbol = "sudo ";
+          swift.symbol = "swift ";
+          typst.symbol = "typst ";
+          vagrant.symbol = "vagrant ";
+          terraform.symbol = "terraform ";
+          xmake.symbol = "xmake ";
+          zig.symbol = "zig ";
+        };
       };
 
       neovim = {
