4a12b2d0c3dc013d6e826706ea4df8d34f9f676b
vereis <me@vereis.com>
2026-01-27 19:39:38 +0000

    fix(nvim): update treesitter config for new API and add tree-sitter CLI
    nvim-treesitter underwent a full rewrite and the old API
    (require('nvim-treesitter.configs').setup) no longer exists.
    
    The new API requires:
    - tree-sitter CLI to be installed for parser compilation
    - Using require('nvim-treesitter').setup({}) and .install()
    - Manually enabling highlighting via vim.treesitter.start()

--- modules/home/tui/neovim/lua/plugins/telescope.lua
+++ modules/home/tui/neovim/lua/plugins/telescope.lua
@@ -2,7 +2,6 @@ return {
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
