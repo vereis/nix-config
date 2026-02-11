d4fe1320c9d01bf6085f6a99e5a6610e6986246b
vereis <me@vereis.com>
2026-02-11 15:01:11 +0000

    fix(mana): persist openclaw state across reboots

--- hosts/linux/mana/default.nix
+++ hosts/linux/mana/default.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   lib,
+  pkgs,
   ...
 }:
 
@@ -21,9 +22,31 @@
     };
   };
 
-  users.users.vereis.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMG37Ow3/XMK/R4TLn6eJkuv6GlVUPoKHb7niULgtKxV"
+  users.users.vereis = {
+    linger = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMG37Ow3/XMK/R4TLn6eJkuv6GlVUPoKHb7niULgtKxV"
+    ];
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /home/vereis/.openclaw 0700 vereis users - -"
+    "d /home/vereis/.openclaw/workspace 0700 vereis users - -"
+    "d /home/vereis/.openclaw/workspace/memory 0700 vereis users - -"
   ];
+
+  systemd.services.openclaw-state-permissions = {
+    description = "Ensure OpenClaw state is writable by vereis";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "local-fs.target" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      if [ -d /home/vereis/.openclaw ]; then
+        ${lib.getExe' pkgs.coreutils "chown"} -R vereis:users /home/vereis/.openclaw
+        ${lib.getExe' pkgs.coreutils "chmod"} -R u+rwX,go-rwx /home/vereis/.openclaw
+      fi
+    '';
+  };
 
   nix = {
     optimise.automatic = lib.mkForce false;
@@ -39,6 +62,11 @@
         image = "rw-store.img";
         mountPoint = "/nix/.rw-store";
         size = 2048;
+      }
+      {
+        image = "openclaw-state.img";
+        mountPoint = "/home/vereis/.openclaw";
+        size = 4096;
       }
     ];
 
